3e865cf0a1ced611cafb01aa7a4cb5eba2260184
Jonathan Penny <jonathan.penny@gmail.com>
2016-11-20 16:59:23 +0000

    Misc Cleanup
    Misc cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -2,100 +2,88 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
+var autoPrefixer = require('gulp-autoprefixer');
+var sourceMaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
-var useref = require('gulp-useref');
+var useRef = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
+var cssNano = require('gulp-cssnano');
+var imageMin = require('gulp-magemin');
 var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
 
-// Development Tasks 
-// -----------------
-
-// Start browserSync server
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
     browserSync({
         server: {
             baseDir: './public'
         }
-    })
+    });
 });
 
-gulp.task('sass', function() {
-    return gulp.src('./resources/assets/scss/**/*.scss') // Gets all files ending with .scss in ./resources/assets/scss and children dirs
-        .pipe(sass()) // Passes it through a gulp-sass
-        .pipe(gulp.dest('./resources/assets/css')) // Outputs it in the css folder
-        .pipe(browserSync.reload({ // Reloading with Browser Sync
+gulp.task('sass', function () {
+    return gulp.src('./resources/assets/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(autoPrefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourceMaps.write('./'))
+        .pipe(gulp.dest('./resources/assets/css'))
+        .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-// Watchers
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('./resources/assets/scss/**/*.scss', ['sass']);
     gulp.watch('./resources/assets/*.html', browserSync.reload);
     gulp.watch('./resources/assets/js/**/*.js', browserSync.reload);
 });
 
-// Optimization Tasks 
-// ------------------
-
-// Optimizing CSS and JavaScript 
-gulp.task('useref', function() {
-
+gulp.task('useref', function () {
     return gulp.src('./resources/assets/*.html')
-        .pipe(useref())
+        .pipe(useRef())
         .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulpIf('*.css', cssNano()))
         .pipe(gulp.dest('./public/'));
 });
 
-// Optimizing Images 
-gulp.task('images', function() {
+gulp.task('images', function () {
     return gulp.src('./resources/assets/images/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
-        .pipe(cache(imagemin({
-            interlaced: true,
+        .pipe(cache(imageMin({
+            interlaced: true
         })))
-        .pipe(gulp.dest('./public/images'))
+        .pipe(gulp.dest('./public/images'));
 });
 
-// Copying fonts 
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
     return gulp.src('./resources/assets/fonts/**/*')
-        .pipe(gulp.dest('./public/fonts'))
+        .pipe(gulp.dest('./public/fonts'));
 });
 
-// Cleaning 
-gulp.task('clean', function() {
-    return del.sync('./public/').then(function(cb) {
+gulp.task('clean', function () {
+    return del.sync('./public/').then(function (cb) {
         return cache.clearAll(cb);
     });
 });
 
-gulp.task('clean:dist', function() {
+gulp.task('clean:dist', function () {
     return del.sync(['./public/**/*', '!./public/images', '!./public/images/**/*']);
 });
 
-// Build Sequences
-// ---------------
-
-gulp.task('default', function(callback) {
+gulp.task('default', function (callback) {
     runSequence(['sass', 'browserSync'], 'watch',
         callback
-    )
+    );
 });
 
-gulp.task('build', function(callback) {
+gulp.task('build', function (callback) {
     runSequence(
         'clean:dist',
         'sass',
         ['useref', 'images', 'fonts'],
         callback
-    )
+    );
 });
