5dfe10ed8934fbe64af0eb6e820cebbe0a0437a3
Jonathan Penny <jonathan.penny@gmail.com>
2016-11-20 16:48:42 +0000

    Added more dependencies
    Added more dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,100 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var cssnano = require('gulp-cssnano');
-var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
 
-gulp.task('sass', function () {
-    gulp.src('./resources/assets/sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./public/css/'));
+// Development Tasks 
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: './public'
+        }
+    })
 });
 
-gulp.task('default', function () {
-    gulp.watch('./resources/assets/sass/**/*.scss', ['sass']);
+gulp.task('sass', function() {
+    return gulp.src('./resources/assets/scss/**/*.scss') // Gets all files ending with .scss in ./resources/assets/scss and children dirs
+        .pipe(sass()) // Passes it through a gulp-sass
+        .pipe(gulp.dest('./resources/assets/css')) // Outputs it in the css folder
+        .pipe(browserSync.reload({ // Reloading with Browser Sync
+            stream: true
+        }));
+});
+
+// Watchers
+gulp.task('watch', function() {
+    gulp.watch('./resources/assets/scss/**/*.scss', ['sass']);
+    gulp.watch('./resources/assets/*.html', browserSync.reload);
+    gulp.watch('./resources/assets/js/**/*.js', browserSync.reload);
+});
+
+// Optimization Tasks 
+// ------------------
+
+// Optimizing CSS and JavaScript 
+gulp.task('useref', function() {
+
+    return gulp.src('./resources/assets/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('./public/'));
+});
+
+// Optimizing Images 
+gulp.task('images', function() {
+    return gulp.src('./resources/assets/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+        .pipe(cache(imagemin({
+            interlaced: true,
+        })))
+        .pipe(gulp.dest('./public/images'))
+});
+
+// Copying fonts 
+gulp.task('fonts', function() {
+    return gulp.src('./resources/assets/fonts/**/*')
+        .pipe(gulp.dest('./public/fonts'))
+});
+
+// Cleaning 
+gulp.task('clean', function() {
+    return del.sync('./public/').then(function(cb) {
+        return cache.clearAll(cb);
+    });
+});
+
+gulp.task('clean:dist', function() {
+    return del.sync(['./public/**/*', '!./public/images', '!./public/images/**/*']);
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'browserSync'], 'watch',
+        callback
+    )
+});
+
+gulp.task('build', function(callback) {
+    runSequence(
+        'clean:dist',
+        'sass',
+        ['useref', 'images', 'fonts'],
+        callback
+    )
 });
